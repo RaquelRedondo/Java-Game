a47ee060b60d2330fa641e65d86550c5c6312de7
RaquelRedondo <raquelro22@gmail.com>
2016-08-24 22:04:23 +0100

    KeyListeners

--- src/Animation.java
+++ src/Animation.java
@@ -11,8 +11,13 @@ public class Animation implements Runnable {
 	public void run(){
 		//Make it continuous
 		while(true){
-			bp.update(g);
-			Thread.sleep(10);
+			bp.update();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 	}
--- src/BlockBreakerPanel.java
+++ src/BlockBreakerPanel.java
@@ -1,12 +1,22 @@
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
-public class BlockBreakerPanel extends JPanel {
+import javafx.scene.input.KeyCode;
+
+public class BlockBreakerPanel extends JPanel implements KeyListener {
 	
 	ArrayList<Block> blocks = new ArrayList<Block>();
+	Block base;
+	
+	Animation animation;
+	Thread thread;
 	
 	BlockBreakerPanel(){
+		
+		base = new Block(170, 500, 70, 20, "rectangle.png");
 		
 		for (int i=0; i<8; i++){
 			blocks.add(new Block((i*72+2), 0, 70, 20, "rectangle.png"));
@@ -18,13 +28,59 @@ public class BlockBreakerPanel extends JPanel {
 			blocks.add(new Block((i*72+2), 60, 70, 20, "rectangle.png"));
 		}
 		
+		//Need these two lines to listen to keys
+		addKeyListener(this);
+		setFocusable(true);
+		
 	}
 
 	@Override
 	public void paintComponent(Graphics g){
+		//Erase screen and reprint
+		super.paintComponent(g);
 		for (Block b : blocks){
 			b.draw(g, this);
 		}
+		
+		base.draw(g,this);
+	}
+	
+	public void update(){
+		repaint();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			animation = new Animation(this);
+			
+			//Start the animation Runnable
+			thread = new Thread(animation);
+			thread.start();
+		}
+		
+		
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			base.x -= 15;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			base.x += 15;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
